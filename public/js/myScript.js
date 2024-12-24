a6bcd99aa2ed9543d5400e3cc0187c99342383b6
Kaiciary <davin.kurnia@matrix-it.co>
2024-12-24 14:31:02 +0700

    fix image

--- public/js/myScript.js
+++ public/js/myScript.js
@@ -2,10 +2,6 @@ var interval = null;
 var dataInterval = 100;
 var iterate = 0;
 
-const canvas = new fabric.Canvas('canvas', {
-  backgroundColor: 'transparent'
-});
-
 var HideControls = {
   'tl': true,
   'tr': false,
@@ -74,13 +70,11 @@ function showData(json) {
       startConfetti();
       download.style.display = 'flex';
       //wish.style.display = 'block';
-      createCanvas();
       // Clear the timeout before it runs (for example, after 2 seconds)
       var myTimeout = setTimeout(function () {
         stopConfetti();
         clearTimeout(myTimeout);
       }, 2000);
-
     }
     var check = document.getElementById('ayat').childNodes[0];
     var check2 = document.getElementById('isi').childNodes[0];
@@ -95,173 +89,44 @@ function showData(json) {
   }, dataInterval);
 }
 
-document.getElementById('downloadButton').addEventListener('click', function () {
-  document.getElementById('downloadButton').style.opacity = 0;
-  document.getElementById('stickerButton').style.opacity = 0;
-
-  const body = document.body;
-  const bodyWidth = document.body.clientWidth;
-  const bodyHeight =document.body.clientHeight;
-
-  // Capture the content of the <body> using html2canvas
-  html2canvas(body,{
-    scale: 1,
-    width: bodyWidth,
-    height: bodyHeight
-  }).then(function (canvas) {
-    // Create an "a" element to trigger the download
-    var link = document.createElement('a');
-    link.href = canvas.toDataURL('image/png');
-    link.download = 'ayatemas.png';
-    link.click();
-    document.getElementById('downloadButton').style.opacity = 1;
-    document.getElementById('stickerButton').style.opacity = 1;
-  });
-});
-
-function createCanvas() {
-  var canvasu = document.getElementById("canvas-wrapper");
-  canvasu.style.display = "inline-block";
-
-  // =================== CANVAS ====================
-
-  canvas.setWidth(document.getElementById("canvas-wrapper").clientWidth);
-  canvas.setHeight(document.getElementById("canvas-wrapper").clientHeight);
-
-  canvas.requestRenderAll();
-}
-
-function addSticker(svgItem) {
-  var group = [];
-
-  fabric.loadSVGFromURL("./public/assets/svg/" + svgItem + ".svg", function (objects, options) {
-    var loadedObjects = new fabric.Group(group);
-    var svg = fabric.util.groupSVGElements(objects, options);
-    svg.setControlsVisibility(HideControls);
-    loadedObjects.set({
-      left: 100,
-      top: 300,
-      width: 60,
-      height: 60,
+async function downloadImage(res) {
+  try {
+    // Make the request to the server to get the image
+    const response = await fetch('http://localhost:3000/download-ayat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: document.getElementById("ayat").innerText,
+        content: document.getElementById("isi").innerText,
+        resolution: res
+      })
     });
 
-    svg.scaleToWidth(100);
-    svg.scaleToHeight(100);
-    canvas.add(loadedObjects);
-    canvas.requestRenderAll();
-    var modal = document.getElementById('myModal');
-    modal.style.display = 'none';
+    // Check if the response is successful
+    if (!response.ok) {
+      throw new Error('Failed to fetch the image');
+    }
 
-  }, function (item, object) {
-    object.set('id', item.getAttribute('id'));
-    group.push(object);
-  }, { crossOrigin: 'anonymous' });
-  canvas.requestRenderAll();
+    // Convert the response to a blob
+    const imageBlob = await response.blob();
+
+    // Create an object URL from the blob
+    const imageUrl = URL.createObjectURL(imageBlob);
+
+    // Optionally, create an anchor element to trigger a download
+    const anchor = document.createElement('a');
+    anchor.href = imageUrl;
+    anchor.download = 'ayat-emas_' + res + '.png';  // Specify the file name
+    anchor.click();  // Trigger the download
+  } catch (error) {
+    console.error('Error downloading the image:', error);
+  }
 }
-// =================== CANVAS ====================
-// =================== ADD TEXT ===================
-
-$('#fill').change(function () {
-  var obj = canvas.getActiveObject();
-
-  if (obj) {
-    // old api
-    // obj.setFill($(this).val());
-    obj.set("fill", this.value);
-  }
-  canvas.renderAll();
-});
-
-$('#font').change(function () {
-  var obj = canvas.getActiveObject();
-
-  if (obj) {
-    obj.set("fontFamily", "MyriadPro");
-  }
-
-  canvas.renderAll();
-});
-
-function addText() {
-  var oText = new fabric.IText('Ketuk dua kali untuk ubah text', {
-    left: 100,
-    top: 300,
-    fill: 'white',
-    fontSize: 24,
-    fontFamily: 'MyriadPro',
-  });
-
-  canvas.add(oText);
-  
-  oText.bringToFront();
-  canvas.setActiveObject(oText);
-  $('#fill, #font').trigger('change');
-  canvas.requestRenderAll();
-  var modal = document.getElementById('myModal');
-  modal.style.display = 'none';
-}
-
-fabric.util.addListener(canvas.upperCanvasEl, 'dblclick', function(e) {
-  console.log('a');
-  if (canvas.findTarget(e)) {
-     let objType = canvas.findTarget(e).type;
-     if (objType === 'i-text') {
-        // Find the IText object
-        let iTextObject = canvas.findTarget(e);
-        if(iTextObject.text == 'Ketuk dua kali untuk ubah text'){
-          // Set the text to an empty string
-        iTextObject.set({ text: '' });
-        }
-        // Render the canvas to apply the changes
-        canvas.renderAll();
-
-        // Show controls
-       // document.getElementById('textControls').hidden = false;
-     }
-  }
-});
-
-
-function addDeleteBtn(x, y) {
-  $(".deleteBtn").remove();
-  var btnLeft = x - 10;
-  var btnTop = y - 10;
-  var deleteBtn = '<img src="http://www.imcjms.com/assets/images/close-window.png" class="deleteBtn" style="position:absolute;top:' + btnTop + 'px;left:' + btnLeft + 'px;cursor:pointer;width:20px;height:20px;"/>';
-  $(".canvas-container").append(deleteBtn);
-}
-
-canvas.on('object:selected', function (e) {
-  addDeleteBtn(e.target.oCoords.tr.x, e.target.oCoords.tr.y);
-});
-
-canvas.on('mouse:down', function (e) {
-  if (!canvas.getActiveObject()) {
-    $(".deleteBtn").remove();
-  }
-});
-
-canvas.on('object:modified', function (e) {
-  addDeleteBtn(e.target.oCoords.tr.x, e.target.oCoords.tr.y);
-});
-
-canvas.on('object:scaling', function (e) {
-  $(".deleteBtn").remove();
-});
-canvas.on('object:moving', function (e) {
-  $(".deleteBtn").remove();
-});
-canvas.on('object:rotating', function (e) {
-  $(".deleteBtn").remove();
-});
-$(document).on('click', ".deleteBtn", function () {
-  if (canvas.getActiveObject()) {
-    canvas.remove(canvas.getActiveObject());
-    $(".deleteBtn").remove();
-  }
-});
 
 //modal
-var openModalBtn = document.getElementById('stickerButton');
+var openModalBtn = document.getElementById('downloadButton');
 var closeModalBtn = document.getElementById('closeModalBtn');
 var modal = document.getElementById('myModal');
 
@@ -271,9 +136,9 @@ openModalBtn.addEventListener('click', function () {
 });
 
 // Close the modal
-closeModalBtn.addEventListener('click', function () {
+/*closeModalBtn.addEventListener('click', function () {
   modal.style.display = 'none';
-});
+});*/
 
 // Close the modal if the user clicks outside of it
 window.addEventListener('click', function (event) {
